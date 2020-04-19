89b53860c703e62f9ee75a1cc8c40e260c685414
KevinMayfield <mayfield.g.kev@gmail.com>
2020-04-19 16:16:18 +0100

    Latest PHE data

--- src/main/java/covid/UKCovidExtractApp.java
+++ src/main/java/covid/UKCovidExtractApp.java
@@ -47,7 +47,7 @@ import org.json.JSONObject;
 public class UKCovidExtractApp implements CommandLineRunner {
 
     String BMD_DEATHS_URL = "https://www.ons.gov.uk/file?uri=/peoplepopulationandcommunity/birthsdeathsandmarriages/deaths/datasets/weeklyprovisionalfiguresondeathsregisteredinenglandandwales/2020/referencetablesweek142020.xlsx";
-    String PHE_JSON_URL = "https://c19pub.azureedge.net/data_202004181457.json";
+    String PHE_JSON_URL = "https://c19pub.azureedge.net/data_202004191500.json";
 
     String NHS_PATHWAYS_URL = "https://files.digital.nhs.uk/7B/F427B2/NHS%20Pathways%20Covid-19%20data%202020-04-16.csv";
     String NHSONLINE_URL = "https://files.digital.nhs.uk/F4/5E629D/111%20Online%20Covid-19%20data_2020-04-16.csv";
